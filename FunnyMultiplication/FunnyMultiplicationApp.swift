9dd40d3ef85356c44337466884d1525c59574284
valchlubnova <valerie@chlubnova.com>
2023-09-09 12:58:11 +0200

    Initial Commit

---
+++ FunnyMultiplication/FunnyMultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunnyMultiplicationApp.swift
+//  FunnyMultiplication
+//
+//  Created by Valerie Chlubnová on 09.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FunnyMultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
